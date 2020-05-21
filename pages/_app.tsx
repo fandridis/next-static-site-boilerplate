5d58ef51dfb66386917989ad7f2196256ab21bf7
Georgios Fandridis <fandridis@gmail.com>
2020-05-21 10:38:53 +0300

    feat: add simple page transition

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import { PageTransition } from 'next-page-transitions';
 import 'styles/reset.scss';
 import 'styles/bootstrap-grid.scss';
 import 'styles/global.scss';
@@ -15,9 +16,33 @@ function MyApp({ Component, pageProps, router }) {
 
   return (
     <Layout {...layoutProps}>
-      <Component {...pageProps} key={router.route} />
+      <PageTransition timeout={300} classNames="page-transition">
+        <Component {...pageProps} key={router.route} />
+      </PageTransition>
+      <style jsx global>{`
+        .page-transition-enter {
+          opacity: 0;
+          transform: translateY(10px);
+        }
+        .page-transition-enter-active {
+          opacity: 1;
+          transform: translateY(0px);
+          transition: ALL 300ms;
+        }
+        .page-transition-exit {
+          opacity: 1;
+          transform: translateY(0px);
+        }
+        .page-transition-exit-active {
+          opacity: 0;
+          transform: translateY(10px);
+          transition: all 300ms;
+        }
+      `}</style>
     </Layout>
+
   );
 }
 
 export default MyApp;
+
